16e7bf75a59296dcc3fa80698d8a788822f7a6aa
michaelleinweaver <mikeleinweaver1@gmail.com>
2015-02-07 12:37:25 -0800

    started conversion from Von Neumann to Harvard architecture

--- CPU.vhd
+++ CPU.vhd
@@ -17,29 +17,37 @@ end CPU;
 
 architecture CPU_arch of CPU is
 	-- signal declaration
-	signal PCUpdate, IorD, MemRead, MemWrite, IRWrite, RegWrite, zero : STD_LOGIC;
+	signal PCUpdate, IorD, InstMemRead, InstMemWrite, DataMemRead, DataMemWrite : STD_LOGIC;
+	signal IRWrite, RegWrite, zero : STD_LOGIC;
 	signal dp_opcode, dp_funct : opcode;
 	signal MemtoReg, RegDst, ALUSrcA, ALUSrcB, PCSource : STD_LOGIC_VECTOR(1 downto 0);
 	signal ALUControl : ALU_opcode;
+	signal addrin_mem : word;
 
 	-- component specification
 	for all : datapath use entity work.datapath(datapath_arch)
-	port map(clk=>clk, reset_N=>reset_N, PCUpdate=>PCUpdate, IorD=>IorD, MemRead=>MemRead,
-						MemWrite=>MemWrite, IRWrite=>IRWrite, MemtoReg=>MemtoReg, RegDst=>RegDst, 
+	port map(clk=>clk, reset_N=>reset_N, PCUpdate=>PCUpdate, IorD=>IorD, InstMemRead=>InstMemRead,
+						InstMemWrite=>InstMemWrite, DataMemRead=>DataMemRead, DataMemWrite=>DataMemWrite,
+						IRWrite=>IRWrite, MemtoReg=>MemtoReg, RegDst=>RegDst, 
 						RegWrite=>RegWrite, ALUSrcA=>ALUSrcA, ALUSrcB=>ALUSrcB, ALUControl=>ALUControl,
 						PCSource=>PCSource, opcode_out=>dp_opcode, func_out=>dp_funct, zero=>zero);
 
 	for all : control use entity work.control(control_arch)
-	port map(clk=>clk, reset_N=>reset_N, opcode_in=>dp_opcode, funct_in=>dp_funct, zero=>zero,
-						PCUpdate=>PCUpdate, IorD=>IorD, MemRead=>MemRead, MemWrite=>MemWrite, IRWrite=>IRWrite,
+	port map(clk=>clk, reset_N=>reset_N, opcode_in=>dp_opcode, funct_in=>dp_funct, zero=>zero, addrin_mem=>addrin_mem,
+						PCUpdate=>PCUpdate, IorD=>IorD, InstMemRead=>InstMemRead, InstMemWrite=>InstMemWrite, 
+						DataMemRead=>DataMemRead, DataMemWrite=>DataMemWrite, IRWrite=>IRWrite,
 						MemtoReg=>MemtoReg, RegDst=>RegDst, RegWrite=>RegWrite, ALUSrcA=>ALUSrcA, ALUSrcB=>ALUSrcB,
 						ALUControl=>ALUControl, PCSource=>PCSource);
 
+	
+
 begin
-	D1: datapath port map(clk, reset_N, PCUpdate, IorD, MemRead, MemWrite, IRWrite, MemtoReg, RegDst,
+	D1: datapath port map(clk, reset_N, PCUpdate, IorD, InstMemRead, InstMemWrite, DataMemRead, DataMemWrite,
+												IRWrite, MemtoReg, RegDst,
 												RegWrite, ALUSrcA, ALUSrcB, ALUControl, PCSource, dp_opcode, dp_funct,
 												zero);
 
-	C1: control port map(clk, reset_N, dp_opcode, dp_funct, zero, PCUpdate, IorD, MemRead, MemWrite, IRWrite,
+	C1: control port map(clk, reset_N, dp_opcode, dp_funct, zero, addrin_mem, PCUpdate, IorD, InstMemRead, InstMemWrite, 
+												DataMemRead, DataMemWrite, IRWrite,
 												MemtoReg, RegDst, RegWrite, ALUSrcA, ALUSrcB, ALUControl, PCSource);
 end CPU_arch;
